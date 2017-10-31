335f4a3155f7bfafb9da79004e46b5d88f706d6c
Armandyne <re.asktom@gmail.com>
2017-11-01 00:30:47 +0600

    initial

---
+++ webscrape.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import demjson
+
+base_url = "https://www.tsb.kz/bankomates"
+
+html = requests.get(base_url).text
+soup = BeautifulSoup(html, "html.parser")
+
+ul = soup.find("ul", {"class":"ul_city"})
+print("ul=", ul)
+
+cities = {}
+for li_a in ul.find_all("a"):
+    print("href=", li_a["href"], ", text=", li_a.text)
+    if li_a.text is not None and len(li_a.text) > 0:
+        cities[li_a.text] = li_a["href"]
+print("length=", len(cities), cities)   
+
+for (city, param) in cities.items():
+    print(city, param)
+    html = requests.get(base_url + param).text     
+    soup = BeautifulSoup(html, "html.parser")
+    script = soup.find("script", text=re.compile("var points = new Array"))
+    
+    if script is not None:
+        # print("script=", script)
+
+        json_text = re.search(r"\s*var points = new Array\s*\(\s*({.*?})\s*\)\s*;\s*", script.string, flags=re.MULTILINE | re.DOTALL).group(1)
+        atm_coord = demjson.decode("[" + json_text + "]")
+        print("length=", len(atm_coord), atm_coord)
